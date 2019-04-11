3e4aa42b55bc4198d505f2b8d3bac89431c6b77d
Saurabh Jain <Jain-Saurabh@hcl.com>
2019-04-11 14:46:19 -0700

    Template automation flow added

---
+++ presentation/AppServiceMigrator/Controllers/TemplateController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppServiceMigrator.Controllers
+{
+    public class TemplateController : Controller
+    {
+        public IActionResult Tier1Configuration()
+        {
+            return View();
+        }
+        public IActionResult Tier2Configuration()
+        {
+            return View();
+        }
+        public IActionResult Tier3Configuration()
+        {
+            return View();
+        }
+        public IActionResult Tier4Configuration()
+        {
+            return View();
+        }
+        public IActionResult Tier5Configuration()
+        {
+            return View();
+        }
+        public IActionResult TemplateReview()
+        {
+            return View();
+        }
+    }
+}
